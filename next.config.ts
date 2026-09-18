2190ff83f154904c5f51e90222bb736e4ad40f7b
FerryFL01 <ferryfebrian.lim@gmail.com>
2026-09-18 17:45:56 +0700

    feat: config and setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  compress: process.env.NODE_ENV !== "development",
 };
 
 export default nextConfig;
